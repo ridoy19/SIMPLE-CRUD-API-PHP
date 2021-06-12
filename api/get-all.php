ed928b8b20e852822bf18efb03eb7ffc6f85f24b
jakaria_ridoy <jh_ridoy@yahoo.com>
2021-06-12 21:30:48 +0600

    Added Pagination In Get All

--- api/get-all.php
+++ api/get-all.php
@@ -10,15 +10,29 @@ require_once('../auth.php');
 
 $isAuthenticated = isAuth();
 
-if ($isAuthenticated) {
+$pageNo = htmlspecialchars(isset($_GET["pageNo"])) ? (int)  htmlspecialchars($_GET["pageNo"]) : 1;
+$pageSize = htmlspecialchars(isset($_GET["pageSize"])) ? (int) htmlspecialchars($_GET["pageSize"]) : 5;
 
-    $sql = 'SELECT * FROM employee';
+
+$startIndex = ($pageNo - 1) * $pageSize;
+
+
+if ($isAuthenticated) {
+    $sql = "SELECT * FROM employee LIMIT $startIndex, $pageSize";
     $result = mysqli_query($connection, $sql);
+
+    $resultTotalCount = mysqli_query($connection, "select count(1) FROM employee");
+    $rows = mysqli_fetch_array($resultTotalCount);
+
+    $totalCount = $rows[0];
+
+    $totalPages = ceil($totalCount / $pageSize);
+    $remainingPages = ceil($totalPages - $pageNo);
 
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_all($result, MYSQLI_ASSOC)) {
-                echo json_encode(array("message" => "Success", "data" => $row));
+                echo json_encode(array("message" => "Success", "data" => $row, "pageNo" => $pageNo, "pageSize" => $pageSize, "totalPages" => $totalPages, "remainingPages" => $remainingPages));
             }
         }else {
             echo json_encode(array('message' => "Not found!", "data" => []));
